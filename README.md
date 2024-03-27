5fe7de9c5a7faeae533a15335d4b46e8cb0a5bf3
Angelo Carly <angelo.carly@protonmail.com>
2024-03-27 13:28:28 +0100

    Add shader compilation

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Hexen
-Trying out Rust and [wgpu](https://github.com/gfx-rs/wgpu)
+Trying out Rust and [wgpu](https://github.com/gfx-rs/wgpu) for creative coding.
+
+Why not [ash](https://github.com/ash-rs/ash)?  
+I tried to setup ash but was intimidated by the vulkan setup I'd have to complete again. Given I want to keep it fun for myself I decided to go for wgpu instead as documentation and implementation wise this seems more developer-friendly.
 
 ## Goals
 - Use compute shaders. [Exmample project](https://github.com/googlefonts/compute-shader-101).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello world");
+use std::default::Default;
+use std::time::Instant;
+use wgpu::InstanceDescriptor;
+
+#[tokio::main]
+async fn main() {
+    let instance = wgpu::Instance::new(InstanceDescriptor::default());
+    let adapter = instance.request_adapter(&Default::default()).await.unwrap();
+    let features = adapter.features();
+    let (device, _queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                required_features: features & wgpu::Features::TIMESTAMP_QUERY,
+                required_limits: Default::default()
+            },
+            None,
+        )
+        .await
+        .unwrap();
+
+    let _query_set = if features.contains(wgpu::Features::TIMESTAMP_QUERY) {
+        Some(device.create_query_set(&wgpu::QuerySetDescriptor {
+            count: 2,
+            ty: wgpu::QueryType::Timestamp,
+            label: None,
+        }))
+    } else {
+        None
+    };
+
+    let start_instant = Instant::now();
+    let _cs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+    });
+    println!("shader compilation {:?}", start_instant.elapsed());
+
 }
