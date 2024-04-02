13048919ca99de718b602c37efac7e3b6ad11bfb
Angelo Carly <angelo.carly@protonmail.com>
2024-04-02 17:03:20 +0200

    Restructure code

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,61 @@ mod image;
 
 use std::default::Default;
 use std::time::Instant;
-use wgpu::InstanceDescriptor;
+use wgpu::{BindGroupLayout, Device, InstanceDescriptor};
 use wgpu::util::DeviceExt;
 use crate::image::Color;
+
+fn create_bind_group_layout(device: &Device) -> wgpu::BindGroupLayout {
+    device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        label: None,
+        entries: &[
+            wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: false },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }, wgpu::BindGroupLayoutEntry {
+                binding: 1,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: false },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+    })
+}
+
+fn create_compute_pipeline(device: &Device, bind_group_layout: &BindGroupLayout) -> wgpu::ComputePipeline {
+    let cs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+    });
+    let compute_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[&bind_group_layout],
+        push_constant_ranges: &[],
+    });
+    device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label: None,
+        layout: Some(&compute_pipeline_layout),
+        module: &cs_module,
+        entry_point: "main",
+    })
+}
 
 #[tokio::main]
 async fn main() {
     const WIDTH: usize = 512;
     const HEIGHT: usize = 512;
 
-    // Shader
+    // Device setup
+
     let instance = wgpu::Instance::new(InstanceDescriptor::default());
     let adapter = instance.request_adapter(&Default::default()).await.unwrap();
     let features = adapter.features();
@@ -27,12 +72,12 @@ async fn main() {
         .await
         .unwrap();
 
-    let mut start_instant = Instant::now();
-    let cs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        label: None,
-        source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-    });
-    println!("shader compilation {:?}", start_instant.elapsed());
+    // Pipeline setup
+
+    let bind_group_layout = create_bind_group_layout(&device);
+    let pipeline = create_compute_pipeline(&device, &bind_group_layout);
+
+    // Buffer setup
 
     let input_f = vec![0f32; 4 * WIDTH * HEIGHT];
     let input: &[u8] = bytemuck::cast_slice(&input_f);
@@ -60,41 +105,7 @@ async fn main() {
             | wgpu::BufferUsages::COPY_SRC,
     });
 
-    // What's a bind group layout? Is it a descriptorsetlayout?
-    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        label: None,
-        entries: &[
-            wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: false },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            }, wgpu::BindGroupLayoutEntry {
-                binding: 1,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: false },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            }],
-    });
-    let compute_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label: None,
-        bind_group_layouts: &[&bind_group_layout],
-        push_constant_ranges: &[],
-    });
-    let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-        label: None,
-        layout: Some(&compute_pipeline_layout),
-        module: &cs_module,
-        entry_point: "main",
-    });
+    // Bind group setup
 
     let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
         label: None,
@@ -108,7 +119,9 @@ async fn main() {
         }],
     });
 
-    start_instant = Instant::now();
+    // Render call
+
+    let mut start_instant = Instant::now();
     let mut encoder = device.create_command_encoder(&Default::default());
     {
         let mut cpass = encoder.begin_compute_pass(&Default::default());
@@ -118,9 +131,10 @@ async fn main() {
     }
     encoder.copy_buffer_to_buffer(&input_buf, 0, &output_buf, 0, input.len() as u64);
     queue.submit(Some(encoder.finish()));
-
     device.poll(wgpu::Maintain::Wait);
     println!("shader execution {:?}", start_instant.elapsed());
+
+    // Parse output
 
     let buf_slice = output_buf.slice(..);
     buf_slice.map_async(wgpu::MapMode::Read, move |_| {});
@@ -129,7 +143,8 @@ async fn main() {
     let data_raw = &*buf_slice.get_mapped_range();
     let data: &[f32] = bytemuck::cast_slice(data_raw);
 
-    // Image
+    // Image output
+
     start_instant = Instant::now();
     let mut color_sink = image::ColorSink::new(WIDTH as u32, HEIGHT as u32);
     for (index, chunk) in data.chunks(4).enumerate() {
